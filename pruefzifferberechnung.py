67dfa8438cabb050b24fb9a4898c3cddad72ab6f
CodingWithLaura <laura.ganss@web.de>
2022-07-20 15:25:45 +0200

    Calculate checksum of all ints in array where index is even

--- pruefzifferberechnung.py
+++ pruefzifferberechnung.py
@@ -2,17 +2,19 @@ def split(word):
     return list(word)
 
 def pruefeID(id):
-    new_ids = []
+    odd_ids = []
+    even_ids = []
     my_strings = []
     string_checksums = []
     int_checksums = []
-    checksum = 0
+    odd_checksum = 0
+    even_checksum = 0
     
-    for i in range(0,len(id),2):
+    for i in range(0,len(id)-1,2):
         id[i] = id[i] * 2
-        new_ids.append(id[i])
+        odd_ids.append(id[i])
         
-    for digit in new_ids:
+    for digit in odd_ids:
         my_strings.append(str(digit))
     print(my_strings)
     
@@ -29,9 +31,26 @@ def pruefeID(id):
     print(int_checksums)
 
     for i in range(0,len(int_checksums)):
-        checksum = checksum  + int_checksums[i]
-    print(checksum)
+        odd_checksum = odd_checksum  + int_checksums[i]
+    print(odd_checksum)
+
+    for i in range(1,len(id)-1,2):
+        even_ids.append(id[i])
+    print(even_ids)
+
+    for i in range(0,len(even_ids)):
+        even_checksum = even_checksum + even_ids[i]
+    print(even_checksum)
+
+    even_odd_checksum = odd_checksum + even_checksum
+
+    print(even_odd_checksum)
         
+    
+
+
+
+    
 def main():
     id = [6,2,5,8,4,3,1,9,7,9]
     pruefeID(id)
