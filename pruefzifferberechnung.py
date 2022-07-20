2804b4721e00a1c5f97125ead4454438ad2b6797
CodingWithLaura <laura.ganss@web.de>
2022-07-20 12:24:02 +0200

    Split array of strings and convert sublist into digits

--- pruefzifferberechnung.py
+++ pruefzifferberechnung.py
@@ -1,6 +1,11 @@
+def split(word):
+    return list(word)
+
 def pruefeID(id):
     new_id = []
     my_strings = []
+    string_checksum = []
+    int_checksum = []
     
     for i in range(0,len(id),2):
         id[i] = id[i] * 2
@@ -8,7 +13,12 @@ def pruefeID(id):
     for digit in new_id:
         my_strings.append(str(digit))
     print(my_strings)
-    
+    for string in range(0,len(my_strings)):
+        string_checksum.append(split(my_strings[string]))
+    for num in string_checksum:
+        for i in num:
+            int_checksum.append(int(i))
+    print(int_checksum)
         
 def main():
     id = [6,2,5,8,4,3,1,9,7,9]
