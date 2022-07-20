d5f3b73f71d7d30416c85ede78f1d8beac192949
CodingWithLaura <laura.ganss@web.de>
2022-07-20 15:53:49 +0200

    Calculate next larger multiple to ten and check if result of calculations matches last digit of id

--- pruefzifferberechnung.py
+++ pruefzifferberechnung.py
@@ -16,44 +16,40 @@ def pruefeID(id):
         
     for digit in odd_ids:
         my_strings.append(str(digit))
-    print(my_strings)
     
     for string in range(0,len(my_strings)):
         string_checksums.append(split(my_strings[string])) 
-    print(string_checksums)
     
     for char_list in string_checksums:
         if len(char_list) > 1:
-            for test in range(0,len(char_list)-1):
-                new_int = int(char_list[test]) + int(char_list[test + 1])
+            for i in range(0,len(char_list)-1):
+                new_int = int(char_list[i]) + int(char_list[i + 1])
                 int_checksums.append(new_int)
-        else: int_checksums.append(int(char_list[test]))
-    print(int_checksums)
+        else: int_checksums.append(int(char_list[i]))
 
     for i in range(0,len(int_checksums)):
         odd_checksum = odd_checksum  + int_checksums[i]
-    print(odd_checksum)
 
     for i in range(1,len(id)-1,2):
         even_ids.append(id[i])
-    print(even_ids)
 
     for i in range(0,len(even_ids)):
         even_checksum = even_checksum + even_ids[i]
-    print(even_checksum)
 
     even_odd_checksum = odd_checksum + even_checksum
 
-    print(even_odd_checksum)
-        
-    
+    larger_multiple = even_odd_checksum + (10 - even_odd_checksum % 10)
 
+    number_to_check = larger_multiple - even_odd_checksum
 
-
+    if number_to_check == id[len(id)-1]:
+        return True
+    else: return False
     
 def main():
     id = [6,2,5,8,4,3,1,9,7,9]
-    pruefeID(id)
+    my_bool = pruefeID(id)
+    print(my_bool)
     
 if __name__ == "__main__":
     main()
