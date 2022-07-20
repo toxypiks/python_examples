41afd2ab2138e3070c7bfebf2707f836d5eeaa02
CodingWithLaura <laura.ganss@web.de>
2022-07-20 11:51:07 +0200

    Convert array of digits into array of strings

--- pruefzifferberechnung.py
+++ pruefzifferberechnung.py
@@ -1,6 +1,14 @@
 def pruefeID(id):
+    new_id = []
+    my_strings = []
+    
     for i in range(0,len(id),2):
-        print(id[i])
+        id[i] = id[i] * 2
+        new_id.append(id[i])
+    for digit in new_id:
+        my_strings.append(str(digit))
+    print(my_strings)
+    
         
 def main():
     id = [6,2,5,8,4,3,1,9,7,9]
