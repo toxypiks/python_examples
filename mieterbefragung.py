ab81aee13eeaefd15607f6afde7f2bcd51c5b7ec
CodingWithLaura <laura.ganss@web.de>
2022-07-21 22:04:10 +0200

    Calculate income group and percentage of rent sorted by income group

--- mieterbefragung.py
+++ mieterbefragung.py
@@ -1,49 +1,31 @@
 einkommen_miete = [[4200,1200],[900,340],[1800,600],[3600,1100],[2700,800],[5900,1300]]
 
-
-
 def prozente(anzahlGruppen,staffelung):
-    #Berechnung prozentualer Anteil der Miete
-    bereich_euro = []
-    einkommen = []
-    test = 0
-    einkommensgruppe = 0
-    einkommensgruppen = []
-    int_einkommensgruppen = []
 
-    for i in range(0,anzahlGruppen-1):
-        test = test + staffelung
-        bereich_euro.append(test)
-    print(bereich_euro)
+    output = []
 
-    for i in range(0,len(einkommen_miete)):
-        einkommen.append(einkommen_miete[i][0])
+    for miete in einkommen_miete:
+        miete[1] = miete[1] /miete[0] * 100
 
-    for value in einkommen:
-        if value/staffelung >= anzahlGruppen:
-            einkommensgruppen.append(str(anzahlGruppen -1))
-        else:
-            einkommensgruppen.append((str(value/staffelung)).split(".")[0])
-    for einkommen in einkommensgruppen:
-        int_einkommensgruppen.append(int(einkommen))
-    print(int_einkommensgruppen)
-
+    for data in einkommen_miete:
+        if data[0]/staffelung >= anzahlGruppen:
+            data[0] = int(anzahlGruppen - 1)
+        else: data[0] = int(data[0]/staffelung)
+        
     #bubble sort kleinste einkommensgruppe zuerst
     tausche = 1
     while(tausche != 0):
         tausche = 0
-        for i in range (0,len(int_einkommensgruppen)-1):
-            if int_einkommensgruppen[i] > int_einkommensgruppen[i+1]:
+        for i in range (0,len(einkommen_miete)-1):
+            if einkommen_miete[i][0] > einkommen_miete[i+1][0]:
                 tausche += 1
-                temp = int_einkommensgruppen[i]
-                int_einkommensgruppen[i] = int_einkommensgruppen[i+1]
-                int_einkommensgruppen[i+1] = temp
-
-    for i in range(0,anzahlGruppen):
-        print("noch nicht fertig")
+                temp = einkommen_miete[i][0]
+                einkommen_miete[i][0] = einkommen_miete[i+1][0]
+                einkommen_miete[i+1][0] = temp
+    print(einkommen_miete)
+    
+    #wenn eine einkommensgruppe doppelt vorkommt, soll sie nicht im output array vorkommen        
                 
-        
-
 def main():
     staffelung = 1000
     anzahlGruppen = 5
