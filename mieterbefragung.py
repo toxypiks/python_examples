3ef2f9f23730250bf697ce42401e4827f68132d4
CodingWithLaura <laura.ganss@web.de>
2022-07-21 13:24:56 +0200

    Acces every first element of 2d array to calculate income group

--- mieterbefragung.py
+++ mieterbefragung.py
@@ -1,17 +1,22 @@
-einkommen_miete = [[4.200,1.200],[900,340],[1.800,600],[3.600,1.100],[2.700,800],[5.900,1.300]]
+einkommen_miete = [[4200,1200],[900,340],[1800,600],[3600,1100],[2700,800],[5900,1300]]
 
 
 
 def prozente(anzahlGruppen,staffelung):
     #Gruppierung der Einkommensgruppen
     #Berechnung prozentualer Anteil der Miete
-    einkommensgruppen = []
+    bereich_euro = []
     test = 0
+    einkommensgruppe = 0
 
-    for i in range(0,anzahlGruppen):
+    for i in range(0,anzahlGruppen-1):
         test = test + staffelung
-        einkommensgruppen.append(test)
-    print(einkommensgruppen)
+        bereich_euro.append(test)
+    print(bereich_euro)
+
+    for i in range(0,len(einkommen_miete)):
+        print(einkommen_miete[i][0])
+        
 
 def main():
     staffelung = 1000
