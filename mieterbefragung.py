b8f445f0235d86bdfd79f1f6891123eae11b0fd4
CodingWithLaura <laura.ganss@web.de>
2022-07-21 14:25:35 +0200

    Write bubble sort algorithm to sort income groups

--- mieterbefragung.py
+++ mieterbefragung.py
@@ -3,13 +3,13 @@ einkommen_miete = [[4200,1200],[900,340],[1800,600],[3600,1100],[2700,800],[5900
 
 
 def prozente(anzahlGruppen,staffelung):
-    #Gruppierung der Einkommensgruppen
     #Berechnung prozentualer Anteil der Miete
     bereich_euro = []
     einkommen = []
     test = 0
     einkommensgruppe = 0
     einkommensgruppen = []
+    int_einkommensgruppen = []
 
     for i in range(0,anzahlGruppen-1):
         test = test + staffelung
@@ -24,7 +24,25 @@ def prozente(anzahlGruppen,staffelung):
             einkommensgruppen.append(str(anzahlGruppen -1))
         else:
             einkommensgruppen.append((str(value/staffelung)).split(".")[0])
-    print(einkommensgruppen)
+    for einkommen in einkommensgruppen:
+        int_einkommensgruppen.append(int(einkommen))
+    print(int_einkommensgruppen)
+
+    #bubble sort kleinste einkommensgruppe zuerst
+    tausche = 1
+    while(tausche != 0):
+        tausche = 0
+        for i in range (0,len(int_einkommensgruppen)-1):
+            if int_einkommensgruppen[i] > int_einkommensgruppen[i+1]:
+                tausche += 1
+                temp = int_einkommensgruppen[i]
+                int_einkommensgruppen[i] = int_einkommensgruppen[i+1]
+                int_einkommensgruppen[i+1] = temp
+
+    for i in range(0,anzahlGruppen):
+        print("noch nicht fertig")
+                
+        
 
 def main():
     staffelung = 1000
