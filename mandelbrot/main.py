554ee11779a6f10d073cfe618b9a51d044cf5723
CodingWithLaura <laura.ganss@web.de>
2023-03-19 17:06:25 +0100

    Add colorization of mandelbrot

--- mandelbrot/main.py
+++ mandelbrot/main.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import matplotlib.pyplot as plt
+from math import log
 
 
 def mandelbrot_point(c ,threshold,iterations):
@@ -15,15 +17,37 @@ def mandelbrot_point(c ,threshold,iterations):
 
 
 def mandelbrot_matrix():
-    image_raw = Image.new(mode="RGB", size=(600, 600))
+    histogram = [0] * 256
+    image_raw = Image.new(mode="RGB", size=(1000, 1000))
     image = image_raw.load()
-    for y_coord in range(0, 600):
-        for x_coord in range(0, 600):
-            x = x_coord/300.0 - 1.5
-            y = y_coord/300.0 - 1
-            value = mandelbrot_point((x,y),2.0,255)
-            image[x_coord, y_coord] = (value, value, value)
+    for y_coord in range(0, 1000):
+        for x_coord in range(0, 1000):
+            x = x_coord/500.0 - 1.5
+            y = y_coord/500.0 - 1
+            value = mandelbrot_point((x,y),2.0, 255)
+            # intensity 0..255
+            #if intensity > 1:
+            #    histogram[intensity] += 1
+            
+            # RGB Value
+            rgb = (0,0,0)
+            if value < 256:
+                # 0 - 255
+                rgb = (0,0,value)
+            elif 255 < value < 511:
+                # 255 < value < 511 
+                rgb = (0, (value - 256), 255 - (value - 256))
+            else:
+                rgb = ((value - 511), 255 - (value - 511), 255 - (value - 511))
+            image[x_coord, y_coord] = rgb
     image_raw.show()
+
+def plot_log_histogram_fct(histogram):
+    for idx in range(0, 255):
+        if histogram[idx] > 0:
+            histogram[idx] = log(histogram[idx])
+    plt.plot(histogram)
+    plt.show()
 
     
 def main():
